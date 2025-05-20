54c8af080eefef9a8cd7d9b0449a80a125acece9
kasacchiful <kasacchiful@gmail.com>
2025-05-20 22:35:08 +0900

    fix cors error

--- backend/public/index.php
+++ backend/public/index.php
@@ -49,7 +49,7 @@ $app = AppFactory::create();
 $app->add(function (Request $request, $handler) {
     $response = $handler->handle($request);
     return $response
-        ->withHeader('Access-Control-Allow-Origin', '*')
+        // ->withHeader('Access-Control-Allow-Origin', '*')
         ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
         ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 });
