58d550bcf1be80de1b542446823509b7bd3fa555
kasacchiful <kasacchiful@gmail.com>
2025-05-30 12:38:09 +0900

    add spot data

--- data/register_dynamodb.sh
+++ data/register_dynamodb.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-jsondata=`yq -o json ./data.yaml`
-echo ${jsondata}
+aws dynamodb execute-transaction \
+  --transact-statements="file://./truncate_table01.json"
+
+jsondata=`yq -o json ./data01.yaml`
+aws dynamodb batch-write-item \
+  --request-items="${jsondata}"
 
 aws dynamodb execute-transaction \
-  --transact-statements="file://./truncate_table.json"
+  --transact-statements="file://./truncate_table02.json"
 
+jsondata=`yq -o json ./data02.yaml`
 aws dynamodb batch-write-item \
   --request-items="${jsondata}"
